27c8f983350dcde2d1961e93b7e27da7a4259f51
anusha5hetty <39473789+anusha5hetty@users.noreply.github.com>
2025-06-17 13:48:11 +0530

    added session

--- mcp-server-demo/utils/shared_mcp.py
+++ mcp-server-demo/utils/shared_mcp.py
@@ -1,13 +1,19 @@
 from mcp.server.fastmcp import FastMCP
 
-# Singleton instance
-_mcp_instance = None
+# # Singleton instance
+# _mcp_instance = FastMCP("PortfolioMCP")
 
-def get_mcp():
-    global _mcp_instance
-    if _mcp_instance is None:
-        _mcp_instance = FastMCP("PortfolioMCP")
-    return _mcp_instance
+# def get_mcp():
+#     global _mcp_instance
+#     if _mcp_instance is None:
+#         _mcp_instance = FastMCP("PortfolioMCP")
+#     return _mcp_instance
 
-# Shortcut for convenience
-mcp = get_mcp()
+mcp = FastMCP("PortfolioMCP")
+def set_mcp(mcp_instance: FastMCP):
+    global mcp
+    mcp = mcp_instance
+    return mcp
+
+class Session:
+    login_cert = None
